e81e3ea507fa5a4254b19d8d21d51db43dcbca0d
Daniel <danielmwambala@gmail.com>
2023-06-02 01:29:16 +0300

    completed calls screen

---
+++ src/data/CallsData.js
@@ -0,0 +1,110 @@
+export const callsData = [
+  {
+    id: 1,
+    name: "Alhama Ndunda",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&usqp=CAU",
+    time: "Today, 23:30",
+    type: "voice",
+    category: "missed",
+  },
+  {
+    id: 2,
+    name: "Leo John",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIKcTkARlljahDz7xR5gq-lwY3NSwsYMQdl_AlXfua4Yc2QcQ9QIG38gxtEiMGNAdoEck&usqp=CAU",
+    time: "Today, 07:58",
+    type: "voice",
+    category: "outgoing",
+  },
+  {
+    id: 3,
+    name: "Ezekiel Mocha",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Today, 08:38",
+    type: "voice",
+    category: "outgoing",
+  },
+  {
+    id: 4,
+    name: "Kephas Brutt",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Today, 07:28",
+    type: "voice",
+    category: "outgoing",
+  },
+  {
+    id: 5,
+    name: "Joan Mercy",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Today, 09:22",
+    type: "video",
+    category: "outgoing",
+  },
+  {
+    id: 6,
+    name: "Robert JJ",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Today, 10:30",
+    type: "video",
+    category: "incoming",
+  },
+  {
+    id: 7,
+    name: "Grace Alagu",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&usqp=CAU",
+    time: "Today, 17:21",
+    type: "video",
+    category: "incoming",
+  },
+  {
+    id: 8,
+    name: "Isaac Rutto",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIKcTkARlljahDz7xR5gq-lwY3NSwsYMQdl_AlXfua4Yc2QcQ9QIG38gxtEiMGNAdoEck&usqp=CAU",
+    time: "Today, 20:56",
+    type: "video",
+    category: "incoming",
+  },
+  {
+    id: 9,
+    name: "Josiah Nenza",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Today, 19:30",
+    type: "voice",
+    category: "incoming",
+  },
+  {
+    id: 10,
+    name: "Mama Ian",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Today, 20:30",
+    type: "video",
+    category: "incoming",
+  },
+  {
+    id: 11,
+    name: "Kirobosi",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Yesterday, 20:45",
+    type: "voice",
+    category: "missed",
+  },
+  {
+    id: 12,
+    name: "Memee",
+    photos:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LeozXy4Idugd5NRZ_tUiC3fyckljKnuFOoKjZhqkCJ9tPmt9Kbc4cZP3Yicl7LMyrnE&usqp=CAU",
+    time: "Yesterday, 20:53",
+    type: "voice",
+    category: "missed",
+  },
+];
--- src/screens/Calls.js
+++ src/screens/Calls.js
@@ -1,12 +1,128 @@
-import { View, Text } from "react-native";
-import React from "react";
-import Wrapper from "../components/global/Wrapper";
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  MaterialCommunityIcons,
+  Fontisto,
+  MaterialIcons,
+} from "react-native-vector-icons";
+import { callsData } from "../data/CallsData";
+import FabButton from "../components/global/FabButton";
+import { Colors } from "../constants/Colors";
 
 const Calls = () => {
+  const [data, setData] = useState(callsData);
+  useEffect(() => {
+    setData(callsData);
+  });
+
+  const callCategory = (type) => {
+    switch (type) {
+      case "missed":
+        return (
+          <MaterialCommunityIcons
+            name="call-received"
+            size={15}
+            color={"red"}
+          />
+        );
+
+      case "incoming":
+        return (
+          <MaterialCommunityIcons
+            name="call-received"
+            size={15}
+            color={Colors.green}
+          />
+        );
+
+      case "outgoing":
+        return (
+          <MaterialCommunityIcons
+            name="call-made"
+            size={15}
+            color={Colors.green}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  const renderItem = useCallback((itemData) => {
+    const { item } = itemData;
+
+    return (
+      <View className="flex-row items-center justify-between my-1 space-x-3">
+        <View className="w-12 h-12 rounded-full">
+          <Image
+            source={{ uri: item.photos }}
+            className="w-full h-full rounded-full bg-gray-300"
+          />
+        </View>
+        <TouchableOpacity className="flex-1 flex-col w-full space-y-1 py-2">
+          <Text className="text-base text-gray-700 font-semibold">
+            {item.name}
+          </Text>
+          <View className="flex-row items-center space-x-1">
+            {callCategory(item.category)}
+            <Text className={`text-xs text-gray-600`}>{item.time}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity className="">
+          <MaterialCommunityIcons
+            name={item.type === "voice" ? "phone" : "video"}
+            size={26}
+            color={Colors.primary}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }, []);
+
+  const openNewChat = () => {};
+  const createStatus = () => {};
+
   return (
-    <Wrapper>
-      <Text>Calls</Text>
-    </Wrapper>
+    <View className="flex-1 px-4">
+      {/* my status */}
+      <View className="flex-row items-center justify-between">
+        <TouchableOpacity
+          onPress={createStatus}
+          className="flex-row flex-1 items-center justify-between my-3 space-x-3"
+        >
+          <View className="bg-theme-color h-10 w-10 rounded-full items-center justify-center">
+            <Fontisto name="link" size={16} color="#FFFFFF" />
+          </View>
+          <View className="flex-1">
+            <Text className="text-lg text-gray-700">Create call link</Text>
+            <Text className="text-gray-700">
+              Share a link for your whatsApp call
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity className="flex items-center justify-center">
+          <Fontisto name="trash" size={16} color="red" />
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-base text-gray-600 my-2">Recent</Text>
+      {/* display the call list */}
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        extraData={data}
+        nestedScrollEnabled={true}
+        maxToRenderPerBatch={3}
+        initialNumToRender={3}
+        showsVerticalScrollIndicator={false}
+      />
+      <FabButton
+        onPressAction={openNewChat}
+        icon={<MaterialIcons name="add-call" size={25} color="#FFFFFF" />}
+      />
+    </View>
   );
 };
 
