f3e564fbc7b72fefd97b1806d449426d40270698
Daniel <danielmwambala@gmail.com>
2023-06-02 00:12:04 +0300

    completed status screen

--- src/screens/Status.js
+++ src/screens/Status.js
@@ -18,7 +18,7 @@ const Status = () => {
     },
     {
       title: "Viewed updates",
-      data: data?.filter((item) => item.viewed === false),
+      data: data?.filter((item) => item.viewed === true),
     },
   ];
 
@@ -26,23 +26,33 @@ const Status = () => {
     const { item } = itemData;
 
     return (
-      <View className="flex-row items-center justify-between my-2 space-x-3">
-        <View className="w-12 h-12 rounded-full">
-          <Image
-            source={{ uri: item.photos }}
-            className="w-full h-full rounded-full bg-gray-300"
-          />
-        </View>
-        <TouchableOpacity className="flex-1 flex-row w-full space-x-1 py-2 items-center">
-          <View className="flex-1">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-base text-gray-700 font-semibold">
-                {item.name}
-              </Text>
-              <Text className={`text-xs text-gray-600`}>{item.time}</Text>
+      <View className="">
+        <Text className="text-base text-gray-600 my-2">{item.title}</Text>
+        <FlatList
+          data={item.data}
+          keyExtractor={(item) => item.id.toString()}
+          extraData={item.data}
+          nestedScrollEnabled={true}
+          maxToRenderPerBatch={3}
+          initialNumToRender={3}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View className="flex-row space-x-3 my-2">
+              <View className="w-12 h-12 rounded-full">
+                <Image
+                  source={{ uri: item.photos }}
+                  className="w-full h-full rounded-full bg-gray-300"
+                />
+              </View>
+              <TouchableOpacity className="flex-1">
+                <Text className="text-base text-gray-700 font-semibold">
+                  {item.name}
+                </Text>
+                <Text className={`text-xs text-gray-600`}>{item.time}</Text>
+              </TouchableOpacity>
             </View>
-          </View>
-        </TouchableOpacity>
+          )}
+        />
       </View>
     );
   }, []);
@@ -74,10 +84,10 @@ const Status = () => {
 
       {/* display the statuses */}
       <FlatList
-        data={data}
-        keyExtractor={(item) => item.id.toString()}
+        data={combinedStatusData}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={renderItem}
-        extraData={data}
+        extraData={combinedStatusData}
         nestedScrollEnabled={true}
         maxToRenderPerBatch={3}
         initialNumToRender={3}
@@ -86,11 +96,7 @@ const Status = () => {
       <FabButton
         onPressAction={viewStatus}
         icon={
-          <MaterialCommunityIcons
-            name="android-messages"
-            size={25}
-            color="#FFFFFF"
-          />
+          <MaterialCommunityIcons name="pencil" size={25} color="#FFFFFF" />
         }
       />
     </View>
